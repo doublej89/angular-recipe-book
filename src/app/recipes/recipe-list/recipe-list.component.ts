eb615c0fc3dfe00d0849e7bb6dc61c274ea06c8b
Jawad Jahangir <jawad.jahangir.666@gmail.com>
2018-07-20 17:58:58 +0600

    basic design for components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,23 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Name1",
+      "blahblahblahblah",
+      "https://upload.wikimedia.org/wikipedia/commons/8/84/Picture_font_awesome.svg"
+    ),
+    new Recipe(
+      "Name2",
+      "blahblahblahblah",
+      "https://upload.wikimedia.org/wikipedia/commons/8/84/Picture_font_awesome.svg"
+    ),
+    new Recipe(
+      "Name3",
+      "blahblahblahblah",
+      "https://upload.wikimedia.org/wikipedia/commons/8/84/Picture_font_awesome.svg"
+    )
+  ];
 
   constructor() {}
 
